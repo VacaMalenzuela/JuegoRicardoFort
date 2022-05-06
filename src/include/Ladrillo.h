afaea66493a13d3c1b3adcb8830e6ccb4313db3e
VacaMalenzuela <macarenavalenz@gmail.com>
2022-05-06 11:07:15 -0500

    Version final

---
+++ src/include/Ladrillo.h
@@ -0,0 +1,31 @@
+#ifndef LADRILLO_H
+#define LADRILLO_H
+#include <SFML\Graphics.hpp>
+#include <SFML\Audio.hpp>
+#include "Collisionable.h"
+#include<list>
+
+class Ladrillo : public sf:: Drawable, public Collisionable
+{
+    public:
+        Ladrillo();
+        //void cmd();
+        //void update();
+        void draw(sf::RenderTarget& target,sf::RenderStates states)const override;
+        sf::FloatRect getBounds()const override;
+        void roto();
+        void Posicion(int);
+       // void desaparece();
+		bool isRoto();
+		void desromper();
+
+    protected:
+        sf::Sprite _ladrillo;
+        sf::Texture _texture;
+        sf::Sound _canal;
+        sf::SoundBuffer _roto;
+        bool esta_roto = false;
+
+};
+
+#endif // LADRILLO_H
